f05105f132e130f8c495c7b68ad68c6aadb8e3a2
Casey Fulton <casey@caseyfulton.com>
2016-02-12 13:24:32 +1030

    Initial commit. Adds script, license, readme and gitignore file.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# nhldl
-A shell script for downloading NHL.tv streams.
+# NHL.DL
+A script for downloading and re-assembling NHL.tv video streams. Won't help you watch NHL streams live.
+
+*NOTE: THIS SCRIPT REQUIRES AN NHL.TV ACCOUNT, OR AT THE VERY LEAST ACCESS TO THE KEYFILES USED TO DECRYPT STREAMS THAT YOU CAN ONLY GET IF YOU HAVE AN ACCOUNT.*
+
+## Message to NHL.tv
+Hi, my name's Casey and I'm a hockey fan who lives in Australia. With the
+recent change in your stream delivery infrastructure, it's made it basically
+impossible for me to watch games. I use a Linux home theatre PC, and the flash
+player doesn't work on it. Also, despite the fact that I have a reasonably ok
+internet connection (for Australia, anyway) the medium quality stream still
+lags horribly. I'm not sure what changed here, because it worked fine on GCL.
+I tend to watch the Habs SportsNet streams - do you put them in Canadian CDNs
+or something? The US streams seem less laggy, but maybe it's a coincidence.
+Anyway this script is utterly useless for watching live games, and really only
+interesting for expats with an NHL.tv account but bad internet, who want to have
+the game waiting for them when they wake up... or get home from work.
+
+## Usage:
+```bash
+$ ./nhldl.sh <master_m3u8_url> <quality>
+```
+
+## Example:
+```bash
+$ ./nhldl.sh "http://hlslive-l3c.med2.med.nhl.com/ls04/nhl/2016/02/10/NHL_GAME_VIDEO_TBLMTL_M2_HOME_20160210/master_wired_web.m3u8" 2500
+```
+
+## Requirements:
+* An NHL.tv account, or access to the keyfiles on NHL servers.
+* Mac or Linux. Sorry Windows users! You could maybe run it through Cygwin?
+* The url for the master m3u8 stream file you want to download.
+* A copy of ffmpeg, or avconv? I can't keep up with that project.
+
+## How do I find the m3u8 file?
+Open the developer tools / inspector in your browser, and watch an nhl.tv
+stream. On the network tab, enter 'm3u8' into the filter. Click on the file
+called something like `master_wired_web.m3u8` for the URL.
+
+## How do I download the keyfiles?
+Each stream has a number of keyfiles that are used to decrypt its segments.
+NHL.tv has (wisely) decided to put these files on a secure server that
+requires authentication. You can either download them manually using your
+browser (the script will tell you their URLs and where to put them), or you
+can export your cookies from your browser to a file called `cookies.txt` in the
+same directory as this script, and they'll be fetched automatically.
+
+If someone wants to figure out how the bloody hell nhl.tv's auth works, we
+could scrape the login process and add user/pass to the script arguments. Submit
+a PR! :D
---
+++ nhldl.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# NHL.DL
+# A script for downloading and assembling NHL.tv video streams. See README.md.
+
+if [ $(uname -s) = "Darwin" ]; then
+    tempfile_cmd="mktemp"
+else
+    tempfile_cmd="tempfile"
+fi
+
+cookies=$($tempfile_cmd)
+login_result_page=$($tempfile_cmd)
+
+# Fetch master m3u8 file
+master_m3u8_url=$1
+master_m3u8_file=$($tempfile_cmd)
+
+stream_directory=$(basename $(dirname $master_m3u8_url))
+
+echo ">>> Fetching master m3u8 file: $master_m3u8_url"
+wget --quiet \
+     --output-document $master_m3u8_file \
+     $master_m3u8_url
+echo ">>> Done."
+
+# Fetch stream m3u8 file
+quality=$2
+stream_m3u8_url="$(dirname $master_m3u8_url)/$(grep ${quality}K $master_m3u8_file)"
+stream_m3u8_file=$($tempfile_cmd)
+
+echo ">>> Fetching $qualityK stream m3u8 file: $stream_m3u8_url"
+wget --quiet \
+     --output-document $stream_m3u8_file \
+     $stream_m3u8_url
+echo ">>> Done."
+
+# Fetch all the keyfiles
+stream_key_urls=$(grep -o "https://mf[^\"]*" $stream_m3u8_file | uniq)
+
+mkdir -p $stream_directory/keys
+
+# @todo: Scrape the nhl.tv login process and add user/pass to script arguments.
+
+if [ -f cookies.txt ]; then
+    echo ">>> Fetching keys using cookies.txt file."
+    while read -r line; do
+        wget --quiet \
+             --load-cookies cookies.txt \
+             --output-document "$stream_directory/keys/$(basename $line)" \
+             $line
+    done <<< "$stream_key_urls"
+    echo ">>> Done."
+else
+    echo ">>> Stream keys:"
+    echo "$stream_key_urls"
+    echo ">>> Download the above keys in your browser and put them in $stream_directory/keys."
+    read -p ">>> Press enter when you're done."
+fi
+
+# @todo: Download all of the stream segments first. Parallelise it.
+
+concat_file=$stream_directory/stream_files.txt
+: > $concat_file
+
+counter=1
+num_of_segments=$(grep "^[^#]" $stream_m3u8_file | wc -l)
+
+# For each line in stream m3u8 file.
+while read -r line; do
+    # If line is a new key and IV
+    if echo $line | grep -q "^#EXT-X-KEY"; then
+        # Extract key.
+        key_url=$(echo "$line" | grep -o "https://mf[^\"]*")
+        key_file="$stream_directory/keys/$(basename $key_url)"
+        key=$(xxd -p $key_file)
+
+        # Extract IV.
+        iv=$(echo "$line" | grep -o "IV=[^$]*" | cut -c 6-)
+
+    # If line is a video segment.
+    elif echo $line | grep -q "^[0-9]"; then
+        # Make directory for segment
+        mkdir -p "$stream_directory/$(dirname $line)"
+
+        # Download segment.
+        echo ">>> Downloading stream segment: $line [$counter/$num_of_segments]"
+        wget --quiet \
+             --output-document "$stream_directory/$line" \
+             $(dirname $stream_m3u8_url)/$line
+        echo ">>> Done."
+
+        # Decrypt segment.
+        echo ">>> Decrypting segment."
+        openssl enc -aes-128-cbc \
+                    -in "$stream_directory/$line" \
+                    -out "$stream_directory/$line.dec" \
+                    -d -K "$key" -iv "$iv"
+        echo ">>> Done."
+
+        # Replace encrypted segment to save disk space.
+        mv "$stream_directory/$line.dec" "$stream_directory/$line"
+
+        # Add segment to ffmpeg concatenation file.
+        echo "file '$line'" >> $concat_file
+
+        # Increment counter
+        counter=$((counter+1))
+    fi
+done < "$stream_m3u8_file"
+
+echo ">>> Concatenating stream."
+ffmpeg -loglevel panic -f concat -i $concat_file -c copy -bsf:a aac_adtstoasc \
+    $stream_directory.mp4
+echo ">>> Finished."
